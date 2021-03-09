bd8954725c3e5b68693d41ca9e239b390396552d
unknown <subhamsbharadwaz@gmail.com>
2021-03-09 11:30:05 +0530

    hash password with bcryptjs

--- controller/auth.js
+++ controller/auth.js
@@ -7,5 +7,14 @@ const User = require('../models/User');
 // @access   Public
 
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  //   Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
   res.status(200).json({ success: true });
 });
