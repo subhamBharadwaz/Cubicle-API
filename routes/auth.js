84e3a294d0cf421c57db848dae1588dfdb255a5c
unknown <subhamsbharadwaz@gmail.com>
2021-03-11 21:55:23 +0530

    logout and clear the cookie

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,14 @@ const {
   getMe,
   updateDetails,
   updatePassword,
+  logout,
 } = require('../controller/auth');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
